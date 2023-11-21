19d27036ce705ba027dfdbd96f4e68b824804ec3
Mnejja-Mariem <151657371+Mnejja-Mariem@users.noreply.github.com>
2023-11-21 19:29:23 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+let dice = 0
+input.onGesture(Gesture.Shake, function () {
+    // We used a variable to store the value of the number picked random
+    dice = randint(1, 6)
+    // We compare the variable dice with each one of the numbers from 1 to 6 and show the convinient face of the dice
+    if (dice == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (dice == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (dice == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (dice == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (dice == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
